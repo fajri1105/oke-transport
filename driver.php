84a6e322fa343e2a99c6cc184159d2d3f1f4b1d5
mfajri115 <fajriaja110505@gmail.com>
2023-06-15 11:00:05 +0700

    membuat desain awal halaman driver dalam file html

--- driver.php
+++ driver.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+    require 'function.php';
     if(!isset($_SESSION["login"])){
         header("Location: masuk.php", true, 301);
         exit();
@@ -8,16 +9,42 @@
         header("Location: pengguna.php", true, 301);
         exit();
     }
+    $email = $_SESSION["email"];
+    $data = bacaWhere("SELECT * FROM user WHERE email = '$email'");
+
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$data["nama"]?></title>
 </head>
 <body>
-    
+    <header>
+        <div class="kiri">
+            <p>Halo <span><?=$data["nama"]?></span></p>
+            <p>Ayo kelola mobil anda!</p>
+        </div>
+        <div class="kanan">
+            <img src="img/logo putih.png" alt="">
+        </div>
+    </header>
+    <section class="data">
+        <div class="kiri">
+            <table>
+                <tr>
+                    <th>Informasi mobil</th>
+                </tr>
+                <tr>
+                    <td>Nama Sopir</td>
+                    <td>:</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </section>
 </body>
 </html>
